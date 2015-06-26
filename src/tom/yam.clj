fa5fe3a3d03f0274520e5d7616785996c040b0bb
Tommi Reiman <tommi@iki.fi>
2015-06-26 14:50:41 +0200

    tom yam is also good.

---
+++ src/tom/yam.clj
@@ -0,0 +1,58 @@
+(ns tom.yam
+  (require [plumbing.core :refer [fnk]]
+           [plumbing.graph :as graph]
+           [plumbing.fnk.pfnk :as pfnk]
+           [clojure.walk :as walk]
+           [schema.core :as s]))
+
+;;
+;; the soup
+;;
+
+(defn soup
+  ([dependencies]
+   (soup dependencies {}))
+  ([dependencies config]
+   ((graph/compile dependencies) {:config config})))
+
+(defn strip [x]
+  (walk/postwalk
+    (fn [x]
+      (if (map? x)
+        (dissoc x s/Keyword)
+        x))
+    x))
+
+(defn dependencies [x]
+  (some-> x pfnk/input-schema strip))
+
+;;
+;; Spike
+;;
+
+(def database* (fnk [[:config [:db url :- String]]]
+                 (println "starting the database!")
+                 {:conn (str "connected to " url)}))
+
+(def http*     (fnk [[:config [:http port :- Long]] routes]
+                 (println "starting the server!")
+                 {:server (str "http-server running at " port ", serving " routes)}))
+
+(def routes*   (fnk [[:db conn]]
+                 (println "compiling the routes!")
+                 (str "a route using the db conn: " conn)))
+
+
+(def config {:db {:url "jdbc:something"}
+             :http {:port 3000}})
+
+(def graph (soup {:db database*
+                  :http http*
+                  :routes routes*} config))
+
+(println "-->")
+(println "web server:" (:db graph))
+(println "database dependencies:" (dependencies database*))
+
+(keys (meta database*))
+(keys (meta http*))
